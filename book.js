b9a89fbd0b6e80fe3fd7fbd023d06d86e1c44264
Nick Hunter <hunter.497@gmail.com>
2019-11-14 07:28:12 -0500

    Adding functionality to remove books and read books.

--- book.js
+++ book.js
@@ -11,6 +11,10 @@ class Book {
   info() {
     return `${this.title}, by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
   }
+
+   toggleReadBook() {
+     this.read = !this.read;
+   }
 }
 
 function addBookToLibrary(book) {
@@ -19,28 +23,47 @@ function addBookToLibrary(book) {
 
 function renderBookList() {
   let bookList = document.querySelector('.book-list');
-  bookList.innerHTML = myLibrary.map(book => {
-    return '<div class="book-card"><div class="book-info">' + book.info() + '</div><button class="delete-book">Remove Book</button></div>';
+  bookList.innerHTML = myLibrary.map((book, index) => {
+    // Would like to clean this up, pass a template in instead of this long string
+    return '<div class="book-card" data-library-index="' + index + '"><div class="book-info">' + book.info() + '</div><button class="read-book">Read Book</button><button class="delete-book">Remove Book</button></div>';
   }).join('');
+
+  let deleteBookButtons = document.querySelectorAll('.delete-book');
+  deleteBookButtons.forEach((element) => {
+    element.addEventListener("click", () => deleteBook(element));
+  });
+
+  let readBookButtons = document.querySelectorAll('.read-book');
+  readBookButtons.forEach((element) => {
+    element.addEventListener("click", () => readBook(element));
+  });
 }
 
 function toggleBookForm() {
   document.querySelector('#newBookForm').classList.toggle('hide');
 }
 
-function deleteBook() {
-  // Delete book here, need to look at the parent of the book button I clicked on, and remove that parent book card and nothing else
+function deleteBook(element) {
+  let bookCard = element.parentNode;
+  myLibrary.splice(bookCard.dataset.libraryIndex, 1);
+  renderBookList();
+}
+
+function readBook(element) {
+  let bookCard = element.parentNode;
+  myLibrary[bookCard.dataset.libraryIndex].read = true;
+  renderBookList();
 }
 
 const newBookFormButton = document.querySelector('#newBookFormButton');
 newBookFormButton.onclick = () => toggleBookForm();
 
-const deleteBookButton = document.querySelector('.delete-book');
-deleteBookButton.onclick = () => deleteBook();
-
 const myBook = new Book('Moby Dick', 'Herman Melville', 585, false);
+const myBook2 = new Book('Moby Dick2', 'Herman Melville', 585, false);
+const myBook3 = new Book('Moby Dick3', 'Herman Melville', 585, false);
+const myBook4 = new Book('Moby Dick4', 'Herman Melville', 585, false);
 addBookToLibrary(myBook);
-addBookToLibrary(myBook);
-addBookToLibrary(myBook);
-addBookToLibrary(myBook);
+addBookToLibrary(myBook2);
+addBookToLibrary(myBook3);
+addBookToLibrary(myBook4);
 renderBookList();
